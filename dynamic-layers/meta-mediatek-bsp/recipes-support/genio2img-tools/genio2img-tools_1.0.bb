EmcraftSystems/meta-toradex-torizon

genio2img-tools: add genio2img/img2genio image conversion scripts
torizoncore-builder customises a plain raw/WIC image, but the Genio build
output is a genio-flash tarball (aiotflash.tar) carrying a sparse system
image, so the tool cannot open it directly.

Add two host-side converters that bracket a torizoncore-builder run:
genio2img unpacks the tarball and unsparses the system image into a plain
WIC, and img2genio re-sparses the customised result and repacks the
tarball with only the rootfs changed. Nothing in between needs a Yocto
build - the user runs a released torizoncore-builder against the staged
image. That image uses 4096-byte sectors, so the run needs
torizoncore-builder's --raw-sector-size option.

The two scripts and an example tcbuild configuration are deployed as a
single archive beside the flashing artifacts, and README-genio.md
documents the conversion flow.

Signed-off-by: Dmitry Starchenko <dmitry.starchenko@emcraft.com>
Signed-off-by: Vladimir Skvortsov <vskvortsov@emcraft.com>

dynamic-layers/meta-mediatek-bsp/recipes-support/genio2img-tools/genio2img-tools_1.0.bb
@@ -1,0 +1,43 @@
+SUMMARY = "Host-side genio/WIC converters for genio-flash images"
+DESCRIPTION = "genio2img and img2genio are standalone tools bracketing a \
+released torizoncore-builder run against a MediaTek genio-flash \
+(aiotflash.tar) image: genio2img unpacks and unsparses the tarball's system \
+WIC, and img2genio re-sparses and repacks the result (no Yocto build needed \
+in between). Deployed as a single genio2img-tools.tar archive alongside the \
+image. Ships the tools plus an example tcbuild configuration."
+HOMEPAGE = "https://github.com/torizon/meta-toradex-torizon"
+
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+PV = "1.0"
+
+SRC_URI = "file://genio2img \
+           file://img2genio \
+           file://tcbuild-genio.yaml \
+"
+
+S = "${WORKDIR}"
+
+inherit deploy
+
+do_configure[noexec] = "1"
+do_compile[noexec] = "1"
+
+# Host-side tools: deploy as one archive beside the flashing artifacts (not
+# into the rootfs), so it travels with the image as a single file to hand off.
+GENIO2IMG_DEPLOY = "${WORKDIR}/deploy-stage/${BPN}"
+
+do_deploy() {
+    install -m 0755 ${S}/genio2img ${GENIO2IMG_DEPLOY}/genio2img
+    install -m 0755 ${S}/img2genio ${GENIO2IMG_DEPLOY}/img2genio
+    install -m 0644 ${S}/tcbuild-genio.yaml ${GENIO2IMG_DEPLOY}/tcbuild-genio.yaml
+    tar --numeric-owner --owner=0 --group=0 --sort=name \
+        --mtime="@${SOURCE_DATE_EPOCH}" \
+        -cf ${DEPLOYDIR}/${BPN}.tar -C ${GENIO2IMG_DEPLOY}/.. ${BPN}
+}
+do_deploy[dirs] += "${GENIO2IMG_DEPLOY}"
+do_deploy[cleandirs] += "${GENIO2IMG_DEPLOY}"
+addtask deploy before do_build after do_compile
+
+COMPATIBLE_MACHINE = "lec-mtk-i1200"

LeoBelmont/meta-torizon-bsp

dynamic-layers/meta-mediatek-bsp/recipes-support/genio2img-tools/genio2img-tools_1.0.bb
@@ -1,0 +1,43 @@
+SUMMARY = "Host-side genio/WIC converters for genio-flash images"
+DESCRIPTION = "genio2img and img2genio are standalone tools bracketing a \
+released torizoncore-builder run against a MediaTek genio-flash \
+(aiotflash.tar) image: genio2img unpacks and unsparses the tarball's system \
+WIC, and img2genio re-sparses and repacks the result (no Yocto build needed \
+in between). Deployed as a single genio2img-tools.tar archive alongside the \
+image. Ships the tools plus an example tcbuild configuration."
+HOMEPAGE = "https://github.com/torizon/meta-torizon-bsp"
+
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+PV = "1.0"
+
+SRC_URI = "file://genio2img \
+           file://img2genio \
+           file://tcbuild-genio.yaml \
+"
+
+S = "${UNPACKDIR}"
+
+inherit deploy
+
+do_configure[noexec] = "1"
+do_compile[noexec] = "1"
+
+# Host-side tools: deploy as one archive beside the flashing artifacts (not
+# into the rootfs), so it travels with the image as a single file to hand off.
+GENIO2IMG_DEPLOY = "${WORKDIR}/deploy-stage/${BPN}"
+
+do_deploy() {
+    install -m 0755 ${S}/genio2img ${GENIO2IMG_DEPLOY}/genio2img
+    install -m 0755 ${S}/img2genio ${GENIO2IMG_DEPLOY}/img2genio
+    install -m 0644 ${S}/tcbuild-genio.yaml ${GENIO2IMG_DEPLOY}/tcbuild-genio.yaml
+    tar --numeric-owner --owner=0 --group=0 --sort=name \
+        --mtime="@${SOURCE_DATE_EPOCH}" \
+        -cf ${DEPLOYDIR}/${BPN}.tar -C ${GENIO2IMG_DEPLOY}/.. ${BPN}
+}
+do_deploy[dirs] += "${GENIO2IMG_DEPLOY}"
+do_deploy[cleandirs] += "${GENIO2IMG_DEPLOY}"
+addtask deploy before do_build after do_compile
+
+COMPATIBLE_MACHINE = "lec-mtk-i1200"