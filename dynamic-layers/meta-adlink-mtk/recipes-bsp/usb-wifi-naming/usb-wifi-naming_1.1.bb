EmcraftSystems/meta-toradex-torizon

usb-wifi-naming: name USB Wi-Fi interfaces after the SMARC USB socket
Assign a persistent interface name according to the following naming
rule:

  SMARC USB0              wlan-usb0        SoC host controller 112a0000
  SMARC USB1              wlan-usb1        SoC host controller 112b0000
  SMARC USB2              wlan-usb2        on-module USB5807C, port 3
  SMARC USB3              wlan-usb3        on-module USB5807C, port 4
  SMARC USB4              wlan-usb4        on-module USB5807C, port 5
  SMARC USB5              wlan-usb5        on-module USB5807C, port 6

An adapter plugged into a hub on a carrier board falls back to
MAC-based naming.

Signed-off-by: Dmitry Starchenko <dmitry.starchenko@emcraft.com>

recipes-bsp/usb-wifi-naming/usb-wifi-naming_1.1.bb
@@ -1,0 +1,38 @@
+SUMMARY = "Socket-tied interface naming for USB Wi-Fi adapters"
+DESCRIPTION = "systemd .link policy naming a USB wireless interface after the SMARC \
+USB signal it is attached to, so the name is fixed to the socket and identical on \
+every unit; MAC-derived naming is kept as the catch-all for adapters attached \
+below those sockets."
+
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+USB_WIFI_LINK_FILES = "60-wlan-usb0.link 61-wlan-usb1.link 62-wlan-usb2.link \
+                       63-wlan-usb3.link 64-wlan-usb4.link 65-wlan-usb5.link \
+                       75-usb-wifi.link"
+
+SRC_URI = "${@' '.join('file://' + f for f in d.getVar('USB_WIFI_LINK_FILES').split())} \
+    file://README \
+"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+# Module-level override token, shared by the UFS and QSPI machines; the .link
+# files encode module wiring, which is valid on either.
+COMPATIBLE_MACHINE = "^lec-mtk1200$"
+INHIBIT_DEFAULT_DEPS = "1"
+
+# nonarch_libdir, not systemd's own rootlibexecdir, which that recipe defines
+# locally and is unset everywhere else.
+do_install() {
+	for f in ${USB_WIFI_LINK_FILES}; do
+		install -Dm 0644 ${WORKDIR}/$f \
+			${D}${nonarch_libdir}/systemd/network/$f
+	done
+	install -Dm 0644 ${WORKDIR}/README ${D}${datadir}/${BPN}/README
+}
+
+FILES:${PN} = "${@' '.join('${nonarch_libdir}/systemd/network/' + f for f in d.getVar('USB_WIFI_LINK_FILES').split())} \
+    ${datadir}/${BPN}/README \
+"
+
+RDEPENDS:${PN} = "udev"

LeoBelmont/meta-torizon-bsp

dynamic-layers/meta-adlink-mtk/recipes-bsp/usb-wifi-naming/usb-wifi-naming_1.1.bb
@@ -1,0 +1,38 @@
+SUMMARY = "Socket-tied interface naming for USB Wi-Fi adapters"
+DESCRIPTION = "systemd .link policy naming a USB wireless interface after the SMARC \
+USB signal it is attached to, so the name is fixed to the socket and identical on \
+every unit; MAC-derived naming is kept as the catch-all for adapters attached \
+below those sockets."
+
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+USB_WIFI_LINK_FILES = "60-wlan-usb0.link 61-wlan-usb1.link 62-wlan-usb2.link \
+                       63-wlan-usb3.link 64-wlan-usb4.link 65-wlan-usb5.link \
+                       75-usb-wifi.link"
+
+SRC_URI = "${@' '.join('file://' + f for f in d.getVar('USB_WIFI_LINK_FILES').split())} \
+    file://README \
+"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+# Module-level override token, shared by the UFS and QSPI machines; the .link
+# files encode module wiring, which is valid on either.
+COMPATIBLE_MACHINE = "^lec-mtk1200$"
+INHIBIT_DEFAULT_DEPS = "1"
+
+# nonarch_libdir, not systemd's own rootlibexecdir, which that recipe defines
+# locally and is unset everywhere else.
+do_install() {
+	for f in ${USB_WIFI_LINK_FILES}; do
+		install -Dm 0644 ${UNPACKDIR}/$f \
+			${D}${nonarch_libdir}/systemd/network/$f
+	done
+	install -Dm 0644 ${UNPACKDIR}/README ${D}${datadir}/${BPN}/README
+}
+
+FILES:${PN} = "${@' '.join('${nonarch_libdir}/systemd/network/' + f for f in d.getVar('USB_WIFI_LINK_FILES').split())} \
+    ${datadir}/${BPN}/README \
+"
+
+RDEPENDS:${PN} = "udev"